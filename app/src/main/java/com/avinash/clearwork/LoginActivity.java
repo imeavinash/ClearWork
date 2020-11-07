763145a3f573c9053b56103d3539af000e4646f6
imeav <imeavinash@gmail.com>
2020-11-07 12:02:13 +0530

    Till checking if contact is also an user

---
+++ app/src/main/java/com/avinash/clearwork/LoginActivity.java
@@ -0,0 +1,194 @@
+package com.avinash.clearwork;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+public class LoginActivity extends AppCompatActivity {
+
+    String tag = "LoginActivity";
+
+    private EditText mPhoneNumber, mCode;
+    private Button mSend;
+
+    private FirebaseAuth mAuth;
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
+
+    String mVerificationId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FirebaseApp.initializeApp(this);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        userIsLoggedIn();
+
+        mPhoneNumber = findViewById(R.id.phoneNumber);
+        mCode = findViewById(R.id.code);
+        mSend = findViewById(R.id.send);
+
+        mSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Log.d(tag,"on click mSens");
+
+                if(mVerificationId != null ){
+
+                    Log.d(tag,"verification id is not null");
+                    verifyPhoneNumberWithCode();
+                }else{
+                    Log.d(tag,"verification id is null");
+                    startPhoneNumberVerification(mPhoneNumber.getText().toString());
+                }
+
+            }
+        });
+
+        mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            @Override
+            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+
+                Log.d("LoginActivity","onVerificationCompleted");
+                signInWithPhoneAuthCredential(phoneAuthCredential);
+            }
+
+            @Override
+            public void onVerificationFailed(@NonNull FirebaseException e) {
+
+                Log.d(tag,"onVerificationFailed");
+
+            }
+
+            @Override
+            public void onCodeSent(@NonNull String verificationId,
+                                   @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken){
+
+                super.onCodeSent(verificationId,forceResendingToken);
+
+                Log.d(tag,"onCodeSent");
+                mVerificationId = verificationId;
+                mSend.setText("Verify Code");
+
+
+
+
+            }
+
+
+        };
+
+
+    }
+
+    private void verifyPhoneNumberWithCode(){
+
+        Log.d("LoginActivity","verifyPhoneNumberWithCode");
+
+        String code = mCode.getText().toString();
+        if(code != null && mVerificationId!=null){
+            PhoneAuthCredential credential = PhoneAuthProvider.getCredential(mVerificationId,mCode.getText().toString());
+            signInWithPhoneAuthCredential(credential);
+        }else{
+            Log.d(tag,"code is null or verification id is null");
+        }
+
+
+    }
+
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential phoneAuthCredential) {
+
+        mAuth.signInWithCredential(phoneAuthCredential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                Log.d(tag,"signinWithCredential onComplete");
+                if(task.isSuccessful()){
+                    Log.d(tag,"task is successful");
+                    final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                    if(user!=null){
+                        final DatabaseReference mUserDB = FirebaseDatabase.getInstance().getReference().child("user").child(user.getUid());
+                        mUserDB.addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                if(!snapshot.exists()){
+
+                                    Map<String, Object> userMap = new HashMap<>();
+                                    userMap.put("phone",user.getPhoneNumber());
+                                    userMap.put("name",user.getPhoneNumber());
+                                    mUserDB.updateChildren(userMap);
+
+                                }
+
+                                userIsLoggedIn();
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+
+                            }
+                        });
+                    }
+
+                }
+
+                //userIsLoggedIn();
+
+            }
+        });
+    }
+
+    private void userIsLoggedIn() {
+
+        Log.d(tag,"userisLoggedIn");
+        FirebaseUser user = mAuth.getCurrentUser();
+        if(user!=null){
+            Log.d("LoginActivity","user is not null");
+            startActivity(new Intent(getApplicationContext(),MainPageActivity.class));
+            finish();
+            return;
+        }
+    }
+
+    private void startPhoneNumberVerification(String phoneNumber) {
+
+        PhoneAuthOptions options =
+                PhoneAuthOptions.newBuilder(mAuth)
+                        .setPhoneNumber(phoneNumber)       // Phone number to verify
+                        .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+                        .setActivity(this)                 // Activity (for callback binding)
+                        .setCallbacks(mCallbacks)          // OnVerificationStateChangedCallbacks
+                        .build();
+        PhoneAuthProvider.verifyPhoneNumber(options);
+    }
+}
---
+++ app/src/main/java/com/avinash/clearwork/MainPageActivity.java
@@ -0,0 +1,51 @@
+package com.avinash.clearwork;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest.permission;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page);
+
+        Button mLogout = findViewById(R.id.logout);
+        Button mFindUser = findViewById(R.id.findUser);
+
+        mFindUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),FindUserActivity.class));
+            }
+        });
+        mLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
+
+        getPermissions();
+    }
+
+    private void getPermissions() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(new String[]{permission.WRITE_CONTACTS, permission.READ_CONTACTS},1);
+        }
+    }
+}
