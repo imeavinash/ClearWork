1a1b6998569a5a658e1873e3a549f2289386275e
imeav <imeavinash@gmail.com>
2020-11-08 15:19:44 +0530

    Send a message - final output - a new message id entry under chat node with text and creator id

--- app/src/main/java/com/avinash/clearwork/Chat/ChatListAdapter.java
+++ app/src/main/java/com/avinash/clearwork/Chat/ChatListAdapter.java
@@ -1,5 +1,7 @@
 package com.avinash.clearwork.Chat;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +11,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.avinash.clearwork.ChatActivity;
 import com.avinash.clearwork.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
@@ -17,10 +20,10 @@ import java.util.ArrayList;
 
 public class ChatListAdapter extends RecyclerView.Adapter<ChatListAdapter.ChatListViewHolder>{
 
-    ArrayList<ChatObject> ChatList;
+    ArrayList<ChatObject> chatList;
 
-    public ChatListAdapter(ArrayList<ChatObject> ChatList){
-        this.ChatList = ChatList;
+    public ChatListAdapter(ArrayList<ChatObject> chatList){
+        this.chatList = chatList;
     }
 
     @NonNull
@@ -35,13 +38,19 @@ public class ChatListAdapter extends RecyclerView.Adapter<ChatListAdapter.ChatLi
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ChatListViewHolder holder, final int position) {
+    public void onBindViewHolder(@NonNull final ChatListViewHolder holder, final int position) {
 
-        holder.mTitle.setText(ChatList.get(position).getChatId());
+        holder.mTitle.setText(chatList.get(position).getChatId());
 
         holder.mLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                Intent intent = new Intent(v.getContext(), ChatActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putString("chatID",chatList.get(holder.getAdapterPosition()).getChatId());
+                intent.putExtras(bundle);
+                v.getContext().startActivity(intent);
 
             }
         });
@@ -50,7 +59,7 @@ public class ChatListAdapter extends RecyclerView.Adapter<ChatListAdapter.ChatLi
 
     @Override
     public int getItemCount() {
-        return ChatList.size();
+        return chatList.size();
     }
 
     public class ChatListViewHolder extends RecyclerView.ViewHolder {
--- app/src/main/java/com/avinash/clearwork/ChatActivity.java
+++ app/src/main/java/com/avinash/clearwork/ChatActivity.java
@@ -5,13 +5,21 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 import com.avinash.clearwork.Chat.ChatObject;
 import com.avinash.clearwork.Chat.MessageAdapter;
 import com.avinash.clearwork.Chat.MessageObject;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ChatActivity extends AppCompatActivity {
 
@@ -21,14 +29,43 @@ public class ChatActivity extends AppCompatActivity {
 
     ArrayList<MessageObject> messageList;
 
+    String chatID;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
+        chatID = getIntent().getExtras().getString("chatID");
+
+        Button mSend = findViewById(R.id.send);
+        mSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendMessage();
+            }
+        });
+
         initializeRecyclerView();
     }
 
+    private void sendMessage(){
+        EditText mMessage = findViewById(R.id.message);
+        if(!mMessage.getText().toString().isEmpty()){
+
+            DatabaseReference newMessageDb = FirebaseDatabase.getInstance().
+                    getReference().child("chat").child(chatID).push();
+
+            Map newMessageMap = new HashMap<>();
+            newMessageMap.put("text",mMessage.getText().toString());
+            newMessageMap.put("creator", FirebaseAuth.getInstance().getUid());
+            newMessageDb.updateChildren(newMessageMap);
+
+
+        }
+
+        mMessage.setText(null);
+    }
     private void initializeRecyclerView() {
 
         messageList = new ArrayList<>();
