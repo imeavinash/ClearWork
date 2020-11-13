499ab63346bb77a7a0939ae1e72af9f05c9268c6
imeav <imeavinash@gmail.com>
2020-11-13 16:18:01 +0530

    Hardcoded Notification to one user

--- app/src/main/java/com/avinash/clearwork/MainPageActivity.java
+++ app/src/main/java/com/avinash/clearwork/MainPageActivity.java
@@ -14,6 +14,7 @@ import android.widget.Button;
 
 import com.avinash.clearwork.Chat.ChatListAdapter;
 import com.avinash.clearwork.Chat.ChatObject;
+import com.avinash.clearwork.Utils.SendNotification;
 import com.facebook.drawee.backends.pipeline.Fresco;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -57,6 +58,7 @@ public class MainPageActivity extends AppCompatActivity {
             }
         });
 
+        new SendNotification("message 1","heading 1", null);
         Button mLogout = findViewById(R.id.logout);
         Button mFindUser = findViewById(R.id.findUser);
 
@@ -69,6 +71,7 @@ public class MainPageActivity extends AppCompatActivity {
         mLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                OneSignal.setSubscription(false);
                 FirebaseAuth.getInstance().signOut();
                 Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
---
+++ app/src/main/java/com/avinash/clearwork/Utils/SendNotification.java
@@ -0,0 +1,24 @@
+package com.avinash.clearwork.Utils;
+
+import com.onesignal.OneSignal;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class SendNotification {
+
+    public SendNotification(String message, String heading, String notificationKey){
+
+        notificationKey = "6b61727f-83d7-4a6c-b687-38cf1ad5b37e";
+        try {
+            JSONObject notificationContent = new JSONObject(
+                    "{'contents':{'en':'" + message + "'},"+
+                            "'include_player_ids':['" + notificationKey + "']," +
+                            "'headings':{'en': '" + heading + "'}}");
+            OneSignal.postNotification(notificationContent, null);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
